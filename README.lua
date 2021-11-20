797957de91a94a882520e383fc19ec1c443b3c15
MCHUB-Trash <94169848+MCHUB-Trash@users.noreply.github.com>
2021-11-20 10:44:19 +0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,6 +1,5 @@
-if game:GetService("CoreGui"):FindFirstChild("Venyx") then
-    game:GetService("CoreGui").Venyx:Destroy()
-end
+
+
 -- venyx ui lib reuploaded by me
 -- init
 local player = game.Players.LocalPlayer
